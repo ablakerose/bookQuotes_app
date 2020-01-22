7b911b5a03762d62ddaed021a0b1e1ef86f82c9a
ablakerose <bdecktor@gmail.com>
2020-01-22 10:46:48 -0500

    use script tags to link js files to index.html file

--- bookQuotes_app_frontend/index.js
+++ bookQuotes_app_frontend/index.js
@@ -1,8 +1,2 @@
-//console.log("linked!");
+const app = new App()
 
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("http://localhost:3000/api/v1/books")
-    .then(res => res.json())
-    .then(data => console.log(data));
-
-});
