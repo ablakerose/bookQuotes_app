4f290cf99a797f22d78a07fff55c8c006b3d8c3d
ablakerose <bdecktor@gmail.com>
2020-01-22 10:44:41 -0500

    create and migrate database

--- bookQuotes_app_frontend/assets/javascript/adapters/bookAdapters.js
+++ bookQuotes_app_frontend/assets/javascript/adapters/bookAdapters.js
@@ -1,1 +1,40 @@
-a class that handles 
+class BooksAdapter {
+    constructor() {
+        this.baseUrl = 'http://localhost:3000/api/v1/books'
+    }
+    
+    getBooks() {
+        return fetch(this.baseUrl).then(res => res.json()
+        )
+    }
+    
+    createBook(value) {
+        const book = {
+            title: value,
+        }
+        return fetch(this.baseUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+                
+              },
+            body: JSON.stringify({ book })
+        }).then(res => res.json())
+    }
+
+    updateBook(value, id) {
+        const book = {
+            title: value,
+        }
+
+        return fetch(`${this.baseUrl}/id`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+                
+              },
+            body: JSON.stringify({ book })
+        }).then(res => res.json())
+    }
+
+}
